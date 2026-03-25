53cd99fca2db7e9baef4931e4a1669c370e5979b
JULIANA PEREIRA LACERDA <00001120681261sp@aluno.educacao.sp.gov.br>
2026-03-25 11:10:00 -0300

    feat: adiciona script de soma simples

---
+++ basico/soma.py
@@ -0,0 +1,5 @@
+num1 = int(input("Digite o primeiro número que deseja somar: "))
+num2 = int(input("Digite o segundo número que deseja somar: "))
+
+soma = num1 + num2
+print(f"A soma dos número é  {soma}")
