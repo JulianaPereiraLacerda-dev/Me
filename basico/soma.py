54e8047e000098441e3cdc0d7c4e7ffad504712c
JULIANA PEREIRA LACERDA <00001120681261sp@aluno.educacao.sp.gov.br>
2026-03-25 11:21:02 -0300

    fix: corrige erro de digitação na mensagem de saída

--- basico/soma.py
+++ basico/soma.py
@@ -1,7 +1,10 @@
 # Atividade de soma
 
+#Inicio
 num1 = int(input("Digite o primeiro número que deseja somar: "))
 num2 = int(input("Digite o segundo número que deseja somar: "))
 
 soma = num1 + num2
 print(f"A soma dos número é  {soma}")
+
+#Fim
